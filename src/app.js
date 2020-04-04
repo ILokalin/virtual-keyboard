10e832d8731c97319a39013bd8e838029cf2ae27
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-04-04 23:09:20 +0500

    feat(app): create container for app

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@ import Keyboard from 'Components/Keyboard';
 
 const display = new Display();
 const keyboard = new Keyboard();
+
 const body = document.querySelector('body');
+const container = document.createElement('div');
+body.classList.add('page');
+container.classList.add('page__container');
+body.append(container);
 
 
 
-display.insert(body);
-keyboard.insert(body);
+display.insert(container);
+keyboard.insert(container);
 display.focus();
