029096e90ea722b663c3950f1e5d86250cc543d1
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-04-06 02:05:12 +0500

    feat(display): create function for type in textarea

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,6 +1,9 @@
 export default class {
   constructor () {
     this.textOut = document.createElement('textarea');
+
+    this.focus = this.focus.bind(this);
+    this.outputKey = this.outputKey.bind(this);
   }
 
   insert(parent) {
@@ -11,4 +14,45 @@ export default class {
     this.textOut.focus();
   }
 
+  controlKeyOperation(code) {
+
+    const operationForFange = (symbol, rangeStart, rangeEnd) => {
+      this.textOut.setRangeText(symbol, rangeStart, rangeEnd, "end");
+    }
+
+    const operation = {
+      backspace: () => {
+        if (this.textOut.selectionStart === this.textOut.selectionEnd) {
+          this.textOut.selectionStart--;
+        } 
+
+        operationForFange('', this.textOut.selectionStart, this.textOut.selectionEnd);
+      },
+      delete: () => {
+        if (this.textOut.selectionStart === this.textOut.selectionEnd) {
+          this.textOut.selectionEnd++;
+        } 
+ 
+        operationForFange('', this.textOut.selectionStart, this.textOut.selectionEnd);
+      },
+      space: () => {
+        operationForFange(' ', this.textOut.selectionStart, this.textOut.selectionEnd);
+      }
+    }
+
+    operation[code]();
+  }
+
+  outputKey(keyObject) {
+    const {control, code, lang} = keyObject;
+    let key = keyObject.eng.key
+
+    if (control) {
+      this.controlKeyOperation(code.toLowerCase());
+    } else {
+      this.textOut.setRangeText(key, this.textOut.selectionStart, this.textOut.selectionEnd, "end");
+    }
+
+    this.focus();
+  }
 }
