28e1505c6352449c52b9f3bc7fa9515eee11afc9
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-04-06 03:34:24 +0500

    feat(app): addition info-panel in app

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 import './app.scss';
 import Display from 'Components/Display';
+import InfoPanel from 'Components/Info-panel';
 import Keyboard from 'Components/Keyboard';
 import Storage from 'Components/Storage';
 
 const storage = new Storage();
 const display = new Display();
+const infoPanel = new InfoPanel();
 
 const keyboard = new Keyboard({
   displayOutput: display.outputKey,
   focusToDispaly: display.focus,
   saveLanguage: storage.saveLanguageHistory,
+  infoPanelLed: infoPanel.led,
   lang: storage.loadLanguageHistory()
   });
 
@@ -22,5 +25,6 @@ body.append(container);
 
 
 display.insert(container);
+infoPanel.insert(container);
 keyboard.insert(container);
 display.focus();
